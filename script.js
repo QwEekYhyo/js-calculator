00e176d236920f0ea425e3e899f4acd045d644d8
QwEekYhyo <qweekyhyo.yt83@gmail.com>
2022-11-15 12:46:35 +0100

    working on screen

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function printOnScreen(thing) {
+    screen = document.getElementById("screen")
+    screen.innerHTML = "<p>" + String(thing) + "</p>";
+}
+
 const op = ["AC", ".", "%", "/", "*", "-", "+"]
 const tchoin = [...Array(10).keys()]
 
@@ -9,3 +14,5 @@ for (let i = keys.length - 1; i >= 0; i--) {
     temp = "<button>" + keys[i] + "</button>"
     document.getElementById("calculator").innerHTML += temp
 }
+
+printOnScreen(69)
