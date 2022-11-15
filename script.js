aefde2805f61139b5bae3bc86bc771f6459e6ec0
QwEekYhyo <qweekyhyo.yt83@gmail.com>
2022-11-15 14:23:48 +0100

    recommandations

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
+const screen = document.getElementById("screen")
+
 function printOnScreen(thing) {
-    screen = document.getElementById("screen")
     screen.innerHTML = "<p>" + String(thing) + "</p>";
 }
 
 const op = ["AC", ".", "%", "/", "*", "-", "+"]
 const tchoin = [...Array(10).keys()]
 
-let keys = tchoin.concat(op)
+const keys = tchoin.concat(op)
 
 let temp
-
-for (let i = keys.length - 1; i >= 0; i--) {
-    temp = "<button>" + keys[i] + "</button>"
-    document.getElementById("calculator").innerHTML += temp
-}
+const div = document.getElementById("calculator")
+div.innerHTML += keys.reverse().map(k => `<button>${k}</button>`).join("");
 
 printOnScreen(69)
