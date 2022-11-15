ba98171fae88322cc58f6cb7450fcd6d7549384e
QwEekYhyo <qweekyhyo.yt83@gmail.com>
2022-11-15 19:41:27 +0100

    you can now do calculations

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+/* GLOBAL VARIABLES */
 const screen = document.getElementById("screen")
 let currentScreen = ""
+let nbMemory
+let opMemory
+/* array with all the keys */
+const op = ["AC", '=',  '.', '%', '/', '*', '-', '+']
+const num = [...Array(10).keys()]
+const keys = num.concat(op)
+
+function calculate(number1, number2, operator) {
+    if (operator == '+') {
+        return number1 + number2
+    } else if (operator == '-') {
+        return number1 - number2
+    } else if (operator == '*') {
+        return number1 * number2
+    } else if (operator == '/') {
+        return number1 / number2
+    } else if (operator == '%') {
+        return number1 % number2
+    }
+}
 
 function printOnScreen(thing) {
     thing = String(thing)
@@ -7,7 +28,7 @@ function printOnScreen(thing) {
     let fsize
     if (len <= 7) {
         fsize = 65
-    } else if (len > 21) {
+    } else if (len >= 21) {
         fsize = 22
     } else {
         fsize = - 2.7 * len + 72
@@ -15,19 +36,42 @@ function printOnScreen(thing) {
     screen.innerHTML = '<p style="font-size :' + String(fsize) + 'px">' + thing + "</p>";
 }
 
-const op = ["AC", ".", "%", "/", "*", "-", "+"]
-const tchoin = [...Array(10).keys()]
+function buttonUse(button) {
+    const data = button.firstChild.data
+    if (data in num || data == '.') {
+        currentScreen += data
+        printOnScreen(Number(currentScreen))
+    } else {
+        if (data == "AC") {
+            currentScreen = ""
+            nbMemory = null
+            opMemory = null
+            printOnScreen(0)
+        } else if (data == '=') {
+            currentScreen = calculate(Number(nbMemory), Number(currentScreen), opMemory)
+            printOnScreen(currentScreen)
+            currentScreen = ""
+            nbMemory = null
+            opMemory = null
+        } else {
+            opMemory = data
+            if (nbMemory == null) {
+                nbMemory = Number(currentScreen)
+                currentScreen = ""
+            }
+        }
+    }
+}
 
-const keys = tchoin.concat(op)
-
+/* generating the HTML with the array above */
 const div = document.getElementById("calculator")
 div.innerHTML += keys.reverse().map(k => `<button>${k}</button>`).join("");
 
+/* assigning click event to buttons */
 let buttons = document.getElementsByTagName("button")
 for (let i in buttons) {
     buttons[i].onclick = function() {
-        currentScreen += buttons[i].firstChild.data
-        printOnScreen(currentScreen)
+        buttonUse(buttons[i])
     }
 }
 
