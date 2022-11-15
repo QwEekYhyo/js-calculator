0cb9298ff673f036a39279488919eac549579377
QwEekYhyo <qweekyhyo.yt83@gmail.com>
2022-11-15 18:10:00 +0100

    we now have kinda responsive font size in screen AND we can click on buttons

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const screen = document.getElementById("screen")
 
 function printOnScreen(thing) {
-    screen.innerHTML = "<p>" + String(thing) + "</p>";
+    thing = String(thing)
+    const len = thing.length
+    let fsize
+    if (len <= 7) {
+        fsize = 65
+    } else if (len > 21) {
+        fsize = 22
+    } else {
+        fsize = - 2.7 * len + 72
+    }
+    screen.innerHTML = '<p style="font-size :' + String(fsize) + 'px">' + thing + "</p>";
 }
 
 const op = ["AC", ".", "%", "/", "*", "-", "+"]
@@ -9,8 +19,14 @@ const tchoin = [...Array(10).keys()]
 
 const keys = tchoin.concat(op)
 
-let temp
 const div = document.getElementById("calculator")
 div.innerHTML += keys.reverse().map(k => `<button>${k}</button>`).join("");
+
+let buttons = document.getElementsByTagName("button")
+for (let i in buttons) {
+    buttons[i].onclick = function() {
+        printOnScreen(buttons[i].firstChild.data)
+    }
+}
 
 printOnScreen(69)
